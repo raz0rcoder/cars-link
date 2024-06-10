919d55ae6983ce76769a4301bd317efad5f516d3
raz0rcoder <34173331+raz0rcoder@users.noreply.github.com>
2024-06-10 16:08:01 -0700

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ]
+  },
+}
+
+export default nextConfig
